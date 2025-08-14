705a698153fd1e476495658c2f799d42b54f06ec
nidia rivera <mirandanidiarivera@gmail.com>
2025-08-14 11:37:32 -0600

    Yo

---
+++ IF2000_introduccionProgra/src/main/java/com/mycompany/if2000_introduccionprogra/IF2000_introduccionProgra.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.if2000_introduccionprogra;
+
+/**
+ *
+ * @author miran
+ */
+public class IF2000_introduccionProgra {
+
+    public static void main(String[] args) {
+       String Name = "Nidia";
+       System.out.println("Hello world");
+       System.out.println("Name:"+Name);
+        
+    } 
+}
---
+++ IF2000_introduccionProgra/src/main/java/domain/Person.java
@@ -0,0 +1,73 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package domain;
+
+/**
+ *
+ * @author miran
+ */
+public class Person {
+    private String name;
+    private String lastname;
+    private String id;
+    private String phone;
+    private String age;
+
+    public Person(String name, String lastname, String id, String phone, String age) {
+        this.name = name;
+        this.lastname = lastname;
+        this.id = id;
+        this.phone = phone;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" + "name=" + getName() + ", lastname=" + getLastname () + ", id=" + getId () + ", phone=" + getPhone () + ", age=" + getAge() + '}';
+    }
+
+  
+    
+}
